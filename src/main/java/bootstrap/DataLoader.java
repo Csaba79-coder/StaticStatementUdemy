d6e9e58ca643bce83b4d36043e618349576bdc15
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2022-03-24 21:46:16 +0100

    good to know

--- src/main/java/bootstrap/DataLoader.java
+++ src/main/java/bootstrap/DataLoader.java
@@ -7,13 +7,13 @@ public class DataLoader {
 
     public void load() {
         StaticTest firstInstance = new StaticTest("1st Instance");
-//      System.out.println(firstInstance.getName() + " is instance number " + StaticTest.getNumInstances());
-//
-//      StaticTest secondInstance = new StaticTest("2nd instance");
-//      System.out.println(secondInstance.getName() + " is instance number " + StaticTest.getNumInstances());
-//
-//      StaticTest thirdInstance = new StaticTest("3rd instance");
-//      System.out.println(thirdInstance.getName() + " is instance number " + StaticTest.getNumInstances());
+        System.out.println(firstInstance.getName() + " is instance number " + StaticTest.getNumInstances());
+
+        StaticTest secondInstance = new StaticTest("2nd instance");
+        System.out.println(secondInstance.getName() + " is instance number " + StaticTest.getNumInstances());
+
+        StaticTest thirdInstance = new StaticTest("3rd instance");
+        System.out.println(thirdInstance.getName() + " is instance number " + StaticTest.getNumInstances());
 
         int answer = MultiplierStatic.multiply(6);
         System.out.println("The answer is " + answer);
